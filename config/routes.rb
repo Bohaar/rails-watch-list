355dd82b63f261add28f4e46c43e6763c0dc6e57
BaharehSammaknejad <baharsammaknejad@gmail.com>
2023-02-23 16:54:00 +0100

    don't see any movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  resources :lists, only: [:index, :show, :new, :create] do
+    resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
 
